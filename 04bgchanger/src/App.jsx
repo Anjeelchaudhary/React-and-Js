01ebb549bf89ea820abb80cffcaecc1349172740
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-12-11 18:45:19 +0545

    baic way to change color ie using function

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState('olive')
+
+  function changeColor(color) {
+    setColor(color)
+  }
+
+  return (
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='fixed flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button onClick={() => changeColor('red')} className=' ouline-none px-4 py-1 rounded-full shadow-lag text-black'>Red</button>
+          <button onClick={() => changeColor('blue')} className=' ouline-none px-4 py-1 rounded-full shadow-lag text-black'>Blue</button>
+          <button onClick={() => { changeColor('green') }} className=' ouline-none px-4 py-1 rounded-full shadow-lag text-black'>Green</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
