899aa57ffff2b0c6a3de723fafa46ce1d60bf65f
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-12-12 20:14:56 +0545

    stuck in error

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
-  const [passwords, setPassword] = useState("");
+  const [password, setPassword] = useState("");
 
   const passwordGeneratot = useCallback(() => {
     let pass = ""
@@ -25,6 +25,25 @@ function App() {
       <h1 className=' text-white text-center my-3'>Password Generator</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
         <input type="text" value={password} className='ouline-none w-full py-1 px-3' placeholder='password' readOnly />
+        <button className='ouline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+          <input type="range" min={6} max={100} value={length} className='cursor-pointer'
+            onChange={(e) => { setLength(e.target.value) }} />
+          <label>Length {length}</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox" defaultChecked={numberAllowed} id="numberInput"
+            onChange={() => { setNumberAllowed((prev) => { !prev }) }} />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox" defaultChecked={charAllowed}
+            onChange={() => { setCharAllowed((prev) => { !prev }) }} />
+          <label htmlFor="characterInput">Character</label>
+        </div>
       </div>
     </div>
   )
