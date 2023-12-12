8091fbc2fd0b7b86e16bf747300b4866f0da260c
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-12-13 01:48:12 +0545

    useCallBack useState, useEffect, useRef

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useState, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -7,25 +7,35 @@ function App() {
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
 
-  const passwordGeneratot = useCallback(() => {
-    let pass = ""
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    if (numberAllowed) str += "0123456789"
-    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
-    for (let i = 1; i <= array.length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1)
-      pass = str.charAt(char)
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char)
     }
     setPassword(pass);
-  }, [length, numberAllowed, charAllowed, setPassword]);
+  }, [length, numberAllowed, charAllowed]);
+
+  const copyPasswordToClipBoard = () => {
+    window.navigator.clipboard.writeText(password);
+    passwordReff.current?.select();
+    // passwordReff.current?.setSelectionRange(0,4)
+  }
+
+  const passwordReff = useRef(null)
+  useEffect(() => { passwordGenerator() }, [length, numberAllowed, charAllowed,])
 
   return (
     <div className='w-full max-w-md mx-auto shadow-md
     rounded-1g px-4 py-3 my-8 bg-gray-800 text-orange-500'>
       <h1 className=' text-white text-center my-3'>Password Generator</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-        <input type="text" value={password} className='ouline-none w-full py-1 px-3' placeholder='password' readOnly />
+        <input ref={passwordReff} type="text" value={password} className='ouline-none w-full py-1 px-3' placeholder='password' readOnly />
         <button className='ouline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+          onClick={copyPasswordToClipBoard}
         >copy</button>
       </div>
       <div className='flex text-sm gap-x-2'>
