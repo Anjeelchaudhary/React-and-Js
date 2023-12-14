4f6aa727dc3e978f1fd0e6818ffb7ef0ad5ca78a
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-12-14 17:55:28 +0545

    basic tailwind note

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
+      style={{ backgroundImage: `url(https://images.pexels.com/photos/1235971/pexels-photo-1235971.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)` }}
+    >
+      <h1 className='bg-white'>Currency Converter</h1>
+    </ div>
+  )
+}
+
+export default App
