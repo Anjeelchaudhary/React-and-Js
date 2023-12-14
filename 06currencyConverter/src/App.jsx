ff4c3fdfe87d365cd97c40b7422a2127f39ff167
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-12-15 00:08:34 +0545

    ui of button changed

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -54,7 +54,8 @@ function App() {
             <div className="relative w-full h-0.5">
               <button
                 type="button"
-                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                // className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'
                 onClick={swap}
               >
                 swap
@@ -70,7 +71,7 @@ function App() {
                 amountDisable
               />
             </div>
-            <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+            <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
               Convert {from.toUpperCase()} to {to.toUpperCase()}
             </button>
           </form>
