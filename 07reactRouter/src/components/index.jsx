8ff690b6e4cccc094b141cd2d69f5932c98e9308
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-12-17 00:48:37 +0545

    webbssite Route

---
+++ 07reactRouter/src/components/index.jsx
@@ -0,0 +1,7 @@
+import About from './About/About'
+import Contact from './Contact/Contacts'
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
+import Home from './Home/Home'
+
+export default { About, Contact, Footer, Header, Home }
