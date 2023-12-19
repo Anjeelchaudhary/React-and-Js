b7f5e5da9227ece52d91a7a4dd7fe3f5c5e358b1
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-12-19 07:41:46 +0545

    how to send data using UserContext

--- 08minioncontext/src/App.jsx
+++ 08minioncontext/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
       <div>React with anjeel</div>
-    </>
+    </UserContextProvider>
   )
 }
 
--- 08minioncontext/src/context/UserContextProvider.jsx
+++ 08minioncontext/src/context/UserContextProvider.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import UserContext from "./userContext";
 
-const UserContextProvider = () => {
+const UserContextProvider = ({ children }) => {
+    const [user, setUser] = React.useState(null);
     return (
-        <>
-
-        </>
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
     )
 }
 
