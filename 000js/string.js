a923ade951894f27d209e2de7b9dafacfc99ec59
anjeelchaudhary <chaudharyanjeel@gmail.com>
2023-12-30 19:13:59 +0545

    string module2

--- 000js/string.js
+++ 000js/string.js
@@ -2,3 +2,9 @@ const myName = "Anjeel"
 const age = 21
 
 console.log(`Hello I am ${myName} and I am currently ${age} yrs old`)
+
+const gameName = new String("anjeel")
+// console.log(gameName)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
