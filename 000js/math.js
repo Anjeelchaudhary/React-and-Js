ff63367864fcec2b38b4329619066c08e32d2302
anjeelchaudhary <chaudharyanjeel@gmail.com>
2024-01-02 17:55:21 +0545

    math in javascript

---
+++ 000js/math.js
@@ -0,0 +1,24 @@
+const marks = 800
+// console.log(marks)
+
+const book = new Number(500)
+// console.log(book)
+
+// console.log(book.toString().length)
+// console.log(book.toFixed(3))
+
+const price = 200.98989
+//toPrecision() e ola method left to right ola side se 3 number k precision di
+// console.log(price.toPrecision(3))
+
+const hello = 1000000
+//ema 0 kaitho ba patta nai chaltho to toLocalString() method use karna e  comma se xutttadehat
+// console.log(hello.toLocaleString('en-IN'))
+//'en-IN' ema indian ola style se ait
+
+//+++++++++++++++ Math s+++++++++++++++++++++++++++++++++++++++ 
+
+// console.log(Math)
+//.abs() e ola method -ve ola value se +ve me convert kardehat
+// console.log(Math.abs(-5))
+console.log(Math.round(4.5))
