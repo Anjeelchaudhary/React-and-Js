6eba6b85d61eb9eb8005fcb5ea90fbf6ffad4994
anjeelchaudhary <chaudharyanjeel@gmail.com>
2024-01-02 18:07:14 +0545

    dates in js

---
+++ 000js/datesinjs.js
@@ -0,0 +1,9 @@
+//dates
+const newDate = new Date();
+// console.log(newDate)
+// console.log(newDate.toLocaleDateString())
+// console.log(newDate.toString())
+// console.log(newDate.toDateString())
+
+//newDate k type yaha object ho
+// console.log(typeof newDate)
