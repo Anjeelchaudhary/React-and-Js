117389cdc5d7dcd1c1d4d2a7059e1ef7e8e05d2b
anjeelchaudhary <chaudharyanjeel@gmail.com>
2024-01-12 17:23:32 +0545

    added all array concepts

--- 000js/02basic/02arrays.js
+++ 000js/02basic/02arrays.js
@@ -13,4 +13,24 @@ const dcHeros = ['superman', 'batman', 'flash']
 // console.log(heros[3][2])
 
 const allHeros = heros.concat(dcHeros)
-console.log(allHeros)
+// console.log(allHeros)
+
+// also good way 
+const all_naya_heros = [...heros, ...dcHeros]
+// console.log(all_naya_heros)
+
+//ekin ekke array me add karebela
+const new_Array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const nNew_Array = new_Array.flat(Infinity)
+// console.log(nNew_Array)
+
+//output
+// [
+//     1, 2, 3, 4, 5,
+//     6, 7, 6, 7, 4,
+//     5
+//   ]
+
+
+//yi mor nau he array me convert kedehat 
+console.log(Array.from("Anjeel"))
