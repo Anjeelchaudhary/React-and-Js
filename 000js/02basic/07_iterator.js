94087ca3a28d1a701602967a18f5cf29ebb6d3f6
anjeelchaudhary <chaudharyanjeel@gmail.com>
2024-01-15 20:04:37 +0545

    forof ani forin iterator for array,map ani objects

---
+++ 000js/02basic/07_iterator.js
@@ -0,0 +1,70 @@
+const { ListSearchIndexesCursor } = require("mongodb")
+
+const arr = [1, 2, 3, 4, 2, 7, 5]
+for (const i of arr) {
+    // console.log(`arrsy : ${i}`)
+}
+
+const greet = "Hello anjeel chaudhary"
+for (const i of greet) {
+    // console.log(`greet : ${i}`)
+}
+
+//maps 
+const map = new Map()
+map.set('np', 'Nepal')
+map.set('jrmn', 'Germany')
+map.set('in', 'India')
+// console.log(map)
+
+for (const [key, value] of map) {
+    // console.log(key, '-', value)
+}
+
+const objects = {
+    'game1': 'pubg',
+    'game2': 'cod'
+}
+
+//nor working in objects for forof loop
+// for (const [key, value] of objects) {
+//     console.log(key, '-', value)
+// }
+
+//objects me iterate karek lak forin loop lagaibo 
+const newObjects = {
+    'jv': 'java',
+    'nd': 'ndoe',
+    'js': 'javascript',
+    'rb': 'ruby',
+    'c': 'c programming'
+}
+for (const i in newObjects) {
+    // console.log(`${i} is ${newObjects[i]}`)
+}
+
+
+// ==========================================================
+// array he iterate kare bela forin loop forin ola loop nai lagaibanat
+// kahe ki forin ola loops key nikaratt jo array k key me 0,1,2,3
+//aisin output data ait tabe array ani map me forof ola loop lena 
+
+//objects me kux vi key dar k kux vi value dare banat tabe forin loop majja se
+//lagaibanat interate kare bela
+
+//====================================================================
+
+//ojbects me forin loop
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut of ${myObject[key]}`);
+}
+//ema value he nikareklak objectname[aniYahaConst me banail ola variabel]
+//ani key he nikarek lak jun sidda for in me banail ola const k variable nau likhna
+
